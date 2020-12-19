7c93ac4ebd44a036dd09a935b2ae628d92495ebc
baishixueyi <1375518791@qq.com>
2020-12-19 16:16:29 +0800

    引入svg图片

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,37 @@
 <template>
     <div>
-        <router-link to="/money">记账</router-link>
+        <router-link to="/money">
+        <svg>
+            <use xlink:href="#money"/>
+        </svg>
+        记账
+        </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+    <svg>
+        <use xlink:href="#labels"/>
+    </svg>
+    标签
+    </router-link>
     |
-    <router-link to="/stat">统计</router-link>
+    <router-link to="/stat">
+    <svg>
+        <use xlink:href="#statistics"/>
+    </svg>
+    统计
+    </router-link>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue'
-
+    const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+    try {
+        importAll(require.context('../assets/icons',true,/\.svg$/));
+    } catch (error) {
+        console.log(error)
+    }
+    
     export default Vue.extend({
         
     })
