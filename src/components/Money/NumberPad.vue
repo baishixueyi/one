e365e5e1d0cbaf49ea3e17a657dbc46cf86d0f7c
bai <bai@MacBook-Air.local>
2021-01-17 18:55:38 +0800

    用js实现vue的第一个组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,37 +1,60 @@
 <template>
-    <div class="write">
-      <ul class="xuan">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-      <input type="text" />
-      <div class="numberPad">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button class="ok">OK</button>
-        <button class="zero">0</button>
-        <button>.</button>
-      </div>
+  <div class="write">
+    <ul class="xuan">
+      <li :class="type === '-' && 'selected'" @click="selectedType('-')">
+        支出
+      </li>
+      <li :class="type === '+' && 'selected'" @click="selectedType('+')">
+        收入
+      </li>
+    </ul>
+    <input type="text" />
+    <div class="numberPad">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>删除</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>清空</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button class="ok">OK</button>
+      <button class="zero">0</button>
+      <button>.</button>
     </div>
+  </div>
 </template>
-
-<script>
-    export default {
-        
-    }
-</script>
+ <script>
+export default {
+  name: "NmberPad",
+  props: ["xxxx"], //外部属性
+  data() {
+    //内部属性
+    return {
+      type: "-", // '-代表支出 +代表收入'
+    };
+  },
+  mounted() {
+    //生命周期函数
+    console.log(this.xxx);
+  },
+  methods: {
+    //内部方法
+    selectedType(type) {
+      if (type !== "-" && type !== "+") {
+        throw new Error("type is nuknown");
+      }
+      this.type = type;
+    },
+  },
+};
+ </script>
 
 <style lang="scss" scoped>
-@import '~@/assets/style/helper.scss';
+@import "~@/assets/style/helper.scss";
 .write {
   margin-top: auto;
   > .xuan {
@@ -94,14 +117,15 @@
       }
       &:nth-child(4),
       &:nth-child(7),
-      &:nth-child(10),
-      &:nth-child(13) {
+      &:nth-child(10){
         background: darken($color, 40%);
       }
       &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(14) {
+      &:nth-child(11){
         background: darken($color, 50%);
+      }
+      &:nth-child(14) {
+        background: darken($color, 55%);
       }
     }
     > .ok {
@@ -112,6 +136,7 @@
     }
     > .zero {
       width: 50%;
+      background: darken($color, 45%);
     }
   }
 }
